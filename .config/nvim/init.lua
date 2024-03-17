e898478aae8f6a3fc976a8fc9a37def18e2fc5d1
Rahsheen Porter <rahsheen.porter@thirtymadison.com>
2024-03-16 21:56:07 -0400

    bring back git search

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -372,6 +372,7 @@ require('lazy').setup {
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+      vim.keymap.set('n', '<leader>sgi', builtin.git_files, { desc = '[S]earch [G]it Files' })
 
       -- Slightly advanced example of overriding default behavior and theme
       vim.keymap.set('n', '<leader>/', function()
