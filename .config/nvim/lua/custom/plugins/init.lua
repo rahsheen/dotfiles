543bab34270a265f7cbe296866d98333a8472618
rahsheen <rahsheen.porter@gmail.com>
2024-12-22 16:25:07 -0500

    switch to blink.cmp and fix conform warning

--- .config/nvim/lua/custom/plugins/init.lua
+++ .config/nvim/lua/custom/plugins/init.lua
@@ -48,6 +48,10 @@ return {
     end,
   },
   {
+    'L3MON4D3/LuaSnip',
+    dependencies = { 'rafamadriz/friendly-snippets' },
+  },
+  {
     'rafamadriz/friendly-snippets',
     config = function()
       require('luasnip.loaders.from_vscode').lazy_load()
