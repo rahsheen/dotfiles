3b1580608479fb4be45e9661cd94600148fa4f90
rahsheen <rahsheen.porter@gmail.com>
2026-01-31 22:32:57 -0500

    add keymaps for neotest

--- .config/nvim/lua/custom/plugins/neotest.lua
+++ .config/nvim/lua/custom/plugins/neotest.lua
@@ -10,7 +10,57 @@ return {
     'olimorris/neotest-rspec',
     'marilari88/neotest-vitest',
   },
-
+  keys = {
+    {
+      '<leader>tr',
+      function()
+        require('neotest').run.run()
+      end,
+      desc = 'Run Nearest',
+    },
+    {
+      '<leader>tf',
+      function()
+        require('neotest').run.run(vim.fn.expand '%')
+      end,
+      desc = 'Run File',
+    },
+    {
+      '<leader>td',
+      function()
+        require('neotest').run.run { strategy = 'dap' }
+      end,
+      desc = 'Debug Nearest',
+    },
+    {
+      '<leader>ts',
+      function()
+        require('neotest').summary.toggle()
+      end,
+      desc = 'Toggle Summary',
+    },
+    {
+      '<leader>to',
+      function()
+        require('neotest').output.open { enter = true, auto_close = true }
+      end,
+      desc = 'Show Output',
+    },
+    {
+      '<leader>tO',
+      function()
+        require('neotest').output_panel.toggle()
+      end,
+      desc = 'Toggle Output Panel',
+    },
+    {
+      '<leader>tS',
+      function()
+        require('neotest').run.stop()
+      end,
+      desc = 'Stop',
+    },
+  },
   config = function()
     require('neotest').setup {
       adapters = {
